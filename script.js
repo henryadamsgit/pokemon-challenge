d3d939931008d1d9ae2bb315b0c0a0dbfd6bb0c1
Henry Adams <henrybruce.adams@icloud.com>
2023-04-05 16:55:04 +0100

    completed string description.

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ import { pokemonArray } from "./data/pokemon.js";
 
 const cards = document.querySelector(".card-container");
 
-const makeCard = ({ id, name, types, sprite }) => {
+const makeCard = ({ name, types, sprite }) => {
   return `<div class="card">
     <img class="card__image" src=${sprite}>
     <div class="card__content">
         <h2 class="card__heading">${name}</h2>
-        <p class="card__text"> ${id}, ${types.join(", ")}</p>
+        <p class="card__text"> ${name} is a ${types.join(", ")} type pokemon</p>
     </div>
   </div>`;
 };
